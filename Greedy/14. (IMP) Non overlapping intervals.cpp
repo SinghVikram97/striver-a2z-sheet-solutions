2ba8ce2ac8c0615f8631e9652fd8c28ca4d1a070
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2024-01-25 10:45:23 -0500

    interval related

---
+++ Greedy/14. (IMP) Non overlapping intervals.cpp
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/non-overlapping-intervals/description/
+// [[1,2],[2,3],[3,4],[1,3]] -> remove [1,3] 
+// sort -> [1,2][1,3][2,3][3,4] -> (1,2) and (1,3) overlap remove (1,3)
+
+// how to decide which one to remove
+// 1st can also be removed for ex.
+// (1,6)(1,2)(4,5) -> remove (1,6) would be min removal
+
+// Greedy? -> sort by end times instead
+
+// (1,2)(4,5)(1,6)
+// (a,b)(c,d)  b will be <=d so c<b will be overlap (1,2)(2,3) ie equal not considered overlap
+
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        vector<pair<int,int> > v;
+
+        for(int i=0;i<intervals.size();i++){
+            v.push_back({intervals[i][0],intervals[i][1]});
+        }
+
+        // sort by end times
+        sort(v.begin(),v.end(),[](const auto &x, const auto &y){
+            if(x.second<y.second){
+                return true;
+            }
+            else if(x.second==y.second){
+                return x.first<y.first;
+            }else{
+                return false;
+            }
+        });
+
+        int prev=v[0].second;
+        int ans=0;
+
+        for(int i=1;i<intervals.size();i++){
+            int c=v[i].first;
+            int b=prev;
+
+            if(c<b){
+                // overlap
+                ans++;
+            }else{
+                // no overlap update prev
+                prev=v[i].second;
+            }
+        }
+
+        return ans;
+    }
+};
