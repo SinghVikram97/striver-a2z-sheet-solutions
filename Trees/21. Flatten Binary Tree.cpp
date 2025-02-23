d77a715b38ae7c1b39f89847734303937ca99ae5
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2025-02-23 14:05:00 -0800

    update

--- Trees/21. Flatten Binary Tree.cpp
+++ Trees/21. Flatten Binary Tree.cpp
@@ -1,23 +1,38 @@
 // https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+
+// VIMP : pass prev as reference
 class Solution {
 public:
-    void helper(TreeNode* root, TreeNode* &prev){
+    void help(TreeNode *root, TreeNode *&prev){
         if(root==NULL){
             return;
         }
-        // pre - root,left,right
-        TreeNode* tempStoringRightChild=root->right;
+
+        // root left right
+
+        // since prev changes root->right 
+        // store it for future
+        TreeNode *rootRight=root->right;
+
+        // connect prev->right to root
         if(prev!=NULL){
-            tempStoringRightChild=root->right;
+            rootRight=root->right;
             prev->right=root;
         }
+
+        // update prev
         prev=root;
-        helper(root->left,prev);
+
+        help(root->left, prev);
+
+        // set root->left as null
         root->left=NULL;
-        helper(tempStoringRightChild,prev);
+
+        // use original right
+        help(rootRight, prev);
     }
     void flatten(TreeNode* root) {
-        TreeNode* prev=NULL;
-        helper(root,prev);
+        TreeNode *prev=NULL;
+        help(root, prev);
     }
 };
