d7754be7b9a923f488628fb57a8924b2ccac62b1
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2026-05-03 16:33:08 -0700

    Update 46. Copy list with random pointer.cpp

--- 18. Amazon/46. Copy list with random pointer.cpp
+++ 18. Amazon/46. Copy list with random pointer.cpp
@@ -52,3 +52,70 @@ public:
         return newHead;
     }
 };
+
+// O(1)
+/*
+Store new nodes in b/w of original list ie.
+
+1->2->3->4->5 : Original list
+
+1->1'->2->2'->3->3' : Original list
+
+random remains same in original list
+
+now we can go from 1 to 4 (ie. it's random and its next would be 4')
+
+
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        Node *cur=head;
+
+        // Insert in b/w
+        while(cur!=NULL){
+            Node *newNode = new Node(cur->val);
+            Node *temp = cur->next;
+            cur->next = newNode;
+            newNode->next=temp;
+            cur=temp;
+        }
+
+        // Add random nodes for copy nodes
+        cur = head;
+        while(cur!=NULL){
+            Node *randomNode = cur->random;
+            Node *copyNode = cur->next;
+
+            if(randomNode!=NULL){
+                copyNode->random = randomNode->next;
+            }
+            cur=cur->next->next;
+        }
+
+
+        // Remove interleaving
+        Node *newHead=NULL;
+        Node *prev=NULL;
+        cur=head;
+        while(cur!=NULL){
+            Node *copyNode = cur->next;
+            // fix original list
+            cur->next=cur->next->next;
+
+            if(prev==NULL){
+                newHead=copyNode;
+                prev=copyNode;
+            }else{
+                prev->next=copyNode;
+                prev=copyNode;
+            }
+
+            // since we fixed next so not cur->next->next here
+            cur=cur->next;
+        }
+
+        return newHead;
+    }
+};
