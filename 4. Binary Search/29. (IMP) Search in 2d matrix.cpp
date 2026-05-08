5d6ba6f28e6d6b093e73fb438a306db17aa1350b
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2026-05-08 10:59:07 -0700

    Update 29. (IMP) Search in 2d matrix.cpp

--- 4. Binary Search/29. (IMP) Search in 2d matrix.cpp
+++ 4. Binary Search/29. (IMP) Search in 2d matrix.cpp
@@ -61,3 +61,27 @@ public:
         return false;
     }
 };
+
+// Same as above
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+
+        int i=0;
+        int j=n-1;
+
+        while(i<m && j>=0){
+            if(matrix[i][j]==target){
+                return true;
+            }else if(target>matrix[i][j]){
+                i++;
+            }else{
+                j--;
+            }
+        }
+
+        return false;
+    }
+};
